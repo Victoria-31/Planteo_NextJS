71b0584dc42d64a6f8eceb3038b65540ce12ef5a
Victoria Fonteny <victoriafonteny@live.fr>
2025-07-15 11:25:19 +0200

    toastify for register/login

--- src/components/register/Register.tsx
+++ src/components/register/Register.tsx
@@ -2,27 +2,33 @@
 
 import { useState } from "react";
 import styles from './Register.module.css';
+import { toast } from "react-toastify";
 
+import { ToastContainer } from 'react-toastify';
 import axios, { AxiosError } from "axios";
 
 export default function Register({ onClose }: { onClose: () => void }) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [name, setName] = useState("");
+  const [error, setError] = useState<string | null>(null);
 
   const handleRegister = async (e: React.FormEvent) => {
     e.preventDefault();
+    setError(null);
+     try {
+    console.log("Registering user:", { email, password, name });
+    await axios.post("/api/auth/signup", { email, password, name });
 
-    try {
-      console.log("Registering user:", { email, password, name });
-      await axios.post("/api/auth/signup", { email, password, name });
-
-      alert("Inscription réussie !");
-      onClose(); 
-    } catch (err: unknown) {
-  const error = err as AxiosError<{ message: string }>;
-  alert(error.response?.data?.message || "Erreur lors de l'inscription");
-}}
+    toast.success("Inscription réussie !");
+    onClose();
+  } catch (err: unknown) {
+      const error = err as AxiosError<{ message: string }>;
+      const errorMessage = error.response?.data?.message || "Erreur lors de l'inscription";
+      setError(errorMessage);
+      toast.error(errorMessage);
+    }
+}
 
   return (
     <form onSubmit={handleRegister} className={styles.registerForm}>
@@ -49,9 +55,22 @@ export default function Register({ onClose }: { onClose: () => void }) {
         required
       />
       <button type="submit" className={styles.buttonSignup}>Créer mon compte</button>
+        <p className="errorMessage">{error}</p>
       <button type="button" className={styles.buttonBackSignin} onClick={onClose}>
         Retour à la connexion
       </button>
+    
+      <ToastContainer
+        position="bottom-center"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={true}
+        closeOnClick
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light" 
+      />
     </form>
   );
 }
